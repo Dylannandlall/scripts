f6ce0b957bd4858c4951a65f847348d6208a7adb
Dylan Nandlall <dan6705@g.rit.edu>
2023-11-20 23:04:51 -0500

    qemu script install

---
+++ QEMU/install-dependencies.sh
@@ -0,0 +1,23 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install python3-venv \
+	python3-pip \
+	libglib2.0-dev \
+	libpixman-1-dev \
+	flex \
+	bison \
+
+
+pip install sphinx 
+
+cd ~Downloads/
+git clone https://github.com/ninja-build/ninja.git && cd ninja
+./configure.py --bootstrap
+cp ninja /usr/bin/
+
+
+wget https://download.qemu.org/qemu-8.2.0-rc0.tar.xz
+tar xvJf qemu-8.2.0-rc0.tar.xz
+cd qemu-8.2.0-rc0
+./configure
+make -j 
